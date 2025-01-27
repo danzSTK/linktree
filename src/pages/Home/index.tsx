8d7534ce86514999e6dfb13195ee5a8ec9d38be0
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2025-01-27 01:45:55 -0300

    Implement social links management; add form for Facebook, Instagram, and YouTube links with Firebase integration

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,85 @@
+import { useEffect, useState } from "react";
 import Social from "../../components/Social";
 import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+
+type LinkProps = {
+  id: string;
+  name: string;
+  url: string;
+  bg: string;
+  color: string;
+};
+
+type SocialLinksProps = {
+  youtube: string;
+  facebook: string;
+  instagram: string;
+};
 
 const Home = () => {
+  const [links, setLinks] = useState<LinkProps[]>([]);
+  const [socialLinks, setSocialLinks] = useState<SocialLinksProps>();
+
+  useEffect(() => {
+    function loadLinks() {
+      const linksRef = collection(db, "links");
+      const queryRef = query(linksRef, orderBy("created", "asc"));
+
+      getDocs(queryRef).then((snapshot) => {
+        const linkList: LinkProps[] = snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            bg: doc.data().bg,
+            color: doc.data().color,
+            name: doc.data().name,
+            url: doc.data().url,
+          };
+        });
+
+        setLinks(linkList);
+      });
+    }
+
+    loadLinks();
+
+    console.log(links);
+  }, []);
+
   return (
     <div className="flex flex-col w-full py-4 items-center justify-center">
-      <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">Daniel Félix </h1>
+      <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">
+        Daniel Félix{" "}
+      </h1>
       <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
 
       <main className="flex flex-col w-11/12 max-w-xl text-center">
-        <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105">
-          <a href="#">
-            <p className="text-base md:text-lg">
-              Canal no youtube
-            </p>
-          </a>
-        </section>
+        {links.map((link) => (
+          <section
+            key={link.id}
+            className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105"
+            style={{ backgroundColor: link.bg }}
+          >
+            <a href={link.url} target="_blank">
+              <p className="text-base md:text-lg" style={{ color: link.color }}>
+                {" "}
+                {link.name}{" "}
+              </p>
+            </a>
+          </section>
+        ))}
 
         <footer className="flex justify-center gap-3 my-4">
-            <Social url="https://www.facebook.com/danielfelix.dev">
-              <FaFacebook size={35} color="#fff"/>
-            </Social>
-            <Social url="https://www.instagram.com/danielfelix.dev/">
-              <FaInstagram size={35} color="#fff"/>
-            </Social>
-            <Social url="https://www.youtube.com/channel/UC1234567890abcdef">
-              <FaYoutube size={35} color="#fff"/>
-            </Social>
+          <Social url="https://www.facebook.com/danielfelix.dev">
+            <FaFacebook size={35} color="#fff" />
+          </Social>
+          <Social url="https://www.instagram.com/danielfelix.dev/">
+            <FaInstagram size={35} color="#fff" />
+          </Social>
+          <Social url="https://www.youtube.com/channel/UC1234567890abcdef">
+            <FaYoutube size={35} color="#fff" />
+          </Social>
         </footer>
       </main>
     </div>
--- src/pages/networks/index.tsx
+++ src/pages/networks/index.tsx
@@ -1,9 +1,99 @@
-const  Networks = () => {
-    return (
-        <div class>
-            <h1>Links</h1>
-        </div>
-    )
-}
+import { FormEvent, useEffect, useState } from "react";
+import Header from "../../components/Header";
+import Input from "../../components/Input";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+
+const Networks = () => {
+  const [facebook, setFacebook] = useState("");
+  const [instagram, setInstagram] = useState("");
+  const [youtube, setYoutube] = useState("");
+
+  type Social = { facebook: string; instagram: string; youtube: string };
+
+  useEffect(() => {
+    const loadLinks = () => {
+      const docRef = doc(db, "social", "link");
+
+      getDoc(docRef).then((doc) => {
+        if (doc.exists()) {
+          const data = doc.data() as Social;
+
+          setFacebook(data.facebook);
+          setInstagram(data.instagram);
+          setYoutube(data.youtube);
+        }
+      });
+
+    };
+    
+    loadLinks();
+  }, []);
+
+  const handleRegister = (e: FormEvent) => {
+    e.preventDefault();
+
+    setDoc(doc(db, "social", "link"), {
+      facebook,
+      instagram,
+      youtube,
+    })
+      .then(() => {
+        alert("Redes sociais salvas com sucesso!");
+      })
+      .catch((error) => {
+        alert("Erro ao salvar as redes sociais!" + error);
+      });
+  };
+
+  return (
+    <div className="flex flex-col items-center min-h-screen pb-7 px-2">
+      <Header />
+
+      <h1 className="text-white text-2xl font-medium mt-8 mb-4">
+        Minhas redes sociais
+      </h1>
+
+      <form onSubmit={handleRegister} className="flex flex-col w-full max-w-xl">
+        <label className="text-white font-medium mt-2 mb-2" htmlFor="">
+          Link do facebook
+        </label>
+        <Input
+          type="url"
+          placeholder="Digite o link do facebook..."
+          value={facebook}
+          onChange={(e) => setFacebook(e.target.value)}
+        />
+
+        <label className="text-white font-medium mt-2 mb-2" htmlFor="">
+          Link do instagram
+        </label>
+        <Input
+          type="url"
+          placeholder="Digite o link do facebook..."
+          value={instagram}
+          onChange={(e) => setInstagram(e.target.value)}
+        />
+
+        <label className="text-white font-medium mt-2 mb-2" htmlFor="">
+          Link do youtube
+        </label>
+        <Input
+          type="url"
+          placeholder="Digite o link do facebook..."
+          value={youtube}
+          onChange={(e) => setYoutube(e.target.value)}
+        />
+
+        <button
+          className="text-white bg-blue-600 h-9 rounded-md items-center justify-center flex mt-3 font-medium"
+          type="submit"
+        >
+          Salvar redes sociais
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Networks;
